153cf527d4d172c1fd58b254700fbc6c056e2791
Joey Chen <jay.hatelove@gmail.com>
2019-12-22 00:17:25 +0800

    redefine overlappingStart

--- src/main/java/Accounting.java
+++ src/main/java/Accounting.java
@@ -48,18 +48,18 @@ public class Accounting {
     }
 
     private long getOverlappingDays(Period period, Budget budget) {
-        LocalDate overlappingStart;
+        LocalDate overlappingStart = period.getStart().isAfter(budget.firstDay()) ? period.getStart() : budget.firstDay();
         LocalDate overlappingEnd;
         if (budget.getYearMonth().equals(period.getStart().format(DateTimeFormatter.ofPattern("yyyyMM")))) {
-            overlappingStart = period.getStart();
+//            overlappingStart = period.getStart();
             overlappingEnd = budget.lastDay();
         }
         else if (budget.getYearMonth().equals(period.getEnd().format(DateTimeFormatter.ofPattern("yyyyMM")))) {
-            overlappingStart = budget.firstDay();
+//            overlappingStart = budget.firstDay();
             overlappingEnd = period.getEnd();
         }
         else {
-            overlappingStart = budget.firstDay();
+//            overlappingStart = budget.firstDay();
             overlappingEnd = budget.lastDay();
         }
         return DAYS.between(overlappingStart, overlappingEnd) + 1;
